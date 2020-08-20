3fe38ddf0f16a059f04dc1d5ac84388b999572c5
Trevor Sharp <sharp.trevor@me.com>
2020-08-20 20:25:17 +0000

    Update server

--- src/server.js
+++ src/server.js
@@ -6,32 +6,5 @@ app.set('port', port);
 var server = http.createServer(app);
 server.listen(port);
 
-const onError = (error) => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
-
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
-const onListening = () => {
-  var addr = server.address();
-  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-  console.log('Listening on ' + bind);
-};
-
-server.on('error', onError);
-server.on('listening', onListening);
+server.on('error', (error) => console.log(error));
+server.on('listening', () => console.log('Server Up and Running'));
